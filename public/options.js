b57ae5c13b285f8ebf7601956674e462e0efc566
m19e <nexus.me7290@gmail.com>
2020-10-01 03:46:01 +0900

    Clean code

--- public/options.js
+++ public/options.js
@@ -1,17 +1,11 @@
 (() => {
-    const restoreAll = () => {
-        document.getElementById("request_url").value = localStorage.getItem("request_url");
-        document.getElementById("username").value = localStorage.getItem("username");
-    };
-
-    const saveAll = () => {
+    const save = () => {
         localStorage.setItem("request_url", document.getElementById("request_url").value);
-        localStorage.setItem("username", document.getElementById("username").value);
     };
 
     const init = () => {
-        restoreAll();
-        document.getElementById("save").addEventListener("click", saveAll);
+        document.getElementById("request_url").value = localStorage.getItem("request_url");
+        document.getElementById("save").addEventListener("click", save);
     };
 
     init();
